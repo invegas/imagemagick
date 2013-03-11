438933fc642cdb50981c8c6c7bd3e9238e10cd3e
liguangyi <liguangyi@liguangyi.(none)>
2013-03-11 16:45:31 +0800

    complete

--- app.js
+++ app.js
@@ -20,31 +20,32 @@ app.get('/', function (req, res) {
 })
 
 app.get('/resize', function (req, res) {
-	var mat = "gif"; 
+	var format = "gif"; 
 	im.resize({
 		srcPath: 'public/images/84.jpg',
-		dstPath: 'public/images/84-small.' + mat,
+		dstPath: 'public/images/84-small.' + format,
 		quality: 1,
   		gravity: "North",
-		format: mat,
+		format: format,
 		width: 800
 	}, function(err, stdout, stderr){
 	  	if (err) throw err;
-	  	res.send('<img src="/images/84-small.' + mat +'">');
+	  	res.send('<img src="/images/84-small.' + format +'">');
 	});
 })
 
 app.get('/crop', function (req, res) {
+	var format = "png";
 	easyimg.crop({
 		src: 'public/images/84.jpg', 
-		dst: 'public/images/84-crop.jpg',
+		dst: 'public/images/84-crop.' + format,
 		cropwidth: 100, 
 		cropheight: 100,
 		x: 10,
 		y: 10,		
 		quality: 100
 	}, function (err, image) {
-		res.send('<img src="/images/84-crop.jpg">');
+		res.send('<img src="/images/84-crop.' + format + '">');
 	})
 })
 
